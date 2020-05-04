c56b05eb88ea21db9c282f00e19ebfeea063bdc8
mattor1000 <ranneyjames90@gmail.com>
2020-05-03 20:12:15 -0400

    Updates

---
+++ precache-manifest.0990407eec807bb31236c15d01fdbfa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12db41d2d98ad1d3fe9ad67a74160388",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "64c781d02f14535008b0",
+    "url": "/tic-tac-toe/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c1c1f5354b209ff3fbc8",
+    "url": "/tic-tac-toe/static/css/main.1d8797b0.chunk.css"
+  },
+  {
+    "revision": "64c781d02f14535008b0",
+    "url": "/tic-tac-toe/static/js/2.0ea64499.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tic-tac-toe/static/js/2.0ea64499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c1f5354b209ff3fbc8",
+    "url": "/tic-tac-toe/static/js/main.c9dd4b30.chunk.js"
+  },
+  {
+    "revision": "a354bf49c71fbe79a9ee",
+    "url": "/tic-tac-toe/static/js/runtime-main.47360e83.js"
+  }
+]);
